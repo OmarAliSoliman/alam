b3579210b257874d0d263b613688689b234a8e08
OmarAliSoliman <49602133+OmarAliSoliman@users.noreply.github.com>
2020-06-10 09:05:16 +0200

    final

--- assets/js/main.js
+++ assets/js/main.js
@@ -26,3 +26,19 @@ function openSearch(){
 function closeSearch(){
   document.getElementById('search-div').style.height = "0";
 }
+
+let boxOpen = false;
+let arrowGetQutoto = document.querySelector("#get-qutu i");
+let qutoBox = document.getElementById('get-qutu');
+arrowGetQutoto.addEventListener('click', ()=>{
+  if(!boxOpen){
+    qutoBox.style.left = "0px";
+    arrowGetQutoto.classList.remove('fa-chevron-right');
+    arrowGetQutoto.classList.add('fa-chevron-left');
+  }else{
+    qutoBox.style.left = "-150px";
+    arrowGetQutoto.classList.remove('fa-chevron-left');
+    arrowGetQutoto.classList.add('fa-chevron-right');
+  }
+  boxOpen = !boxOpen;
+})
